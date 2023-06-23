4253dad1f0841ca3a9c90a1fa922d6caea81ff6c
handro123 <838689+handro123@users.noreply.github.com>
2023-06-23 10:47:15 -0700

    Update E46_Seat_Memory_K-Bus.ino
    Updated selected values to make it easier to understand

--- E46_Seat_Memory_K-Bus.ino
+++ E46_Seat_Memory_K-Bus.ino
@@ -1,10 +1,10 @@
-byte keyInserted [7] = {0x44,0x05,0xBF,0x74,0x04,0x00,0x8E};
-byte keyRemoved [7] = {0x44,0x05,0xBF,0x74,0x00,0xFF,0x75};
-byte ignitionOff [6] = {0x80,0x04,0xBF,0x11,0x00,0x2A};
-byte ignitionAcc [6] = {0x80,0x04,0xBF,0x11,0x01,0x2B};
-byte ignitionIgn [6] = {0x80,0x04,0xBF,0x11,0x03,0x29};
+byte keyInserted [7] = {0x44,0x05,0xBF,0x74,0x04,0x00,0x8E};  // Ignition key in
+byte keyRemoved [7] = {0x44,0x05,0xBF,0x74,0x00,0xFF,0x75};   // Ignition key out
+byte ignitionOff [6] = {0x80,0x04,0xBF,0x11,0x00,0x2A};       // Ignition Off
+byte ignitionAcc [6] = {0x80,0x04,0xBF,0x11,0x01,0x2B};       // Ignition Accessory position - POS1
+byte ignitionIgn [6] = {0x80,0x04,0xBF,0x11,0x03,0x29};       // Ignition On position - POS2
 
-byte selected = 2;
+byte selected = 3;
 uint32_t timer1 = millis();
 
 void setup() {
@@ -18,20 +18,20 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(2) == LOW){selected = 1; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
-  if(digitalRead(3) == LOW){selected = 2; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
-  if(digitalRead(4) == LOW){selected = 3; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
-  if(digitalRead(5) == LOW){selected = 4; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
-  if(digitalRead(6) == LOW){selected = 5; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
+  if(digitalRead(2) == LOW){selected = 2; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
+  if(digitalRead(3) == LOW){selected = 3; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
+  if(digitalRead(4) == LOW){selected = 4; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
+  if(digitalRead(5) == LOW){selected = 5; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
+  if(digitalRead(6) == LOW){selected = 6; digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);}
 
   if(millis() - timer1 > 1000)
   {
     digitalWrite(13, HIGH);
-    if(selected == 1){Serial.write(ignitionOff,6);}
-    if(selected == 2){Serial.write(ignitionAcc,6);}
-    if(selected == 3){Serial.write(ignitionIgn,6);}
-    if(selected == 4){Serial.write(keyRemoved,7);}
-    if(selected == 5){Serial.write(keyInserted,7);}
+    if(selected == 2){Serial.write(ignitionOff,6);}
+    if(selected == 3){Serial.write(ignitionAcc,6);}
+    if(selected == 4){Serial.write(ignitionIgn,6);}
+    if(selected == 5){Serial.write(keyRemoved,7);}
+    if(selected == 6){Serial.write(keyInserted,7);}
     delay(10); digitalWrite(13, LOW);
     timer1 = millis();
   }
